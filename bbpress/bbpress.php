a78ddcfb59fb080713bc1d0bcb5c0c4c35abd4c0
Sceko <info@sceko.com>
2016-11-24 11:29:39 +0100

    Update

--- bbpress/bbpress.php
+++ bbpress/bbpress.php
@@ -87,6 +87,8 @@
 </div>
 </footer>
 
+<?php wp_footer(); ?>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/bbpress/jquery-ias.min.js"></script>
@@ -117,6 +119,6 @@ ias.extension(new IASSpinnerExtension({
         });
 </script>
 
-<?php wp_footer(); ?>
+
 </body>
 </html>
