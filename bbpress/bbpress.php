3d1dc37d5cc51d11a937f177768aed77c6501aa9
Sceko <info@sceko.com>
2016-12-04 10:30:59 +0100

    Update

--- bbpress/bbpress.php
+++ bbpress/bbpress.php
@@ -168,8 +168,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/bbpress/jquery-ias.min.js"></script>
 
-<?php wp_footer(); ?>
-
 <script>
     var ias = jQuery.ias({
         container:  '#content',
